959899ce6a1db0bd9c867d0154eb8a06a0bdd26d
Chetan Nada <call2chetannada@gmail.com>
2024-01-07 23:40:21 +0530

    Added Food Fire Web Server Files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# FoodFire-Server
-A Node.js web server that fetches data from a third-party (Swiggy) API and exposes it to a client (FoodFire) app.
+# FoodFire Web Server
+
+A Node.js web server that fetches data from a third-party (Swiggy) API and exposes it to a client [🚀 FoodFire ](https://foodfire-app.netlify.app/) App 😍.
+
+FoodFire is part of [Namaste React Live Course 🙏](https://learn.namastedev.com/courses/namaste-react-live)
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('cross-fetch');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+// For Restaurant API
+app.get('/api/restaurants', async (req, res) => {
+    const { lat, lng, page_type } = req.query;
+    console.log(req.query);
+
+    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=${page_type}`;
+
+    await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('An error occurred');
+        });
+});
+
+
+// For Menu API
+app.get('/api/menu', async (req, res) => {
+    const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng, submitAction, restaurantId } = req.query;
+    console.log(req.query);
+
+    const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
+
+
+    await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('An error occurred');
+        });
+});
+
+
+app.get('/', (req, res) => {
+    res.json({ "test": "Welcome to FireFood!" });
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
